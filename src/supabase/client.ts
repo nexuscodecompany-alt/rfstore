dab9ae1fec649a8e7fba530e6fd30c7d02172778
nexuscodecompany-alt <nexuscode.company@gmail.com>
2026-06-02 11:46:05 -0300

    algo

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -4,4 +4,11 @@ import { Database } from "./types";
 const supabaseKey = import.meta.env.VITE_SUPABASE_API_KEY;
 const supabaseUrl = import.meta.env.VITE_PROJECT_URL_SUPABASE;
 
-export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
+export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+  },
+});
